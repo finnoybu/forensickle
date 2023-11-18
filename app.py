eb41e967a1d59c267f9f3cb6e1fda05adec6fd4f
Finnoybu <ewestbo@gmail.com>
2023-11-18 11:08:15 -0500

    Add error handlign to app; ignore .vscode folder

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-import tkinter as tk
-from tkinter import ttk
-
-from utilities import user
+import sys
+from modules.admin import is_admin, run_as_admin
 
 
-is_admin = user.is_admin()
-print("is_admin: {}".format(is_admin))
+def main():
+    """
+    This function checks if the script is running with administrative privileges. If not, it attempts to
+    run the script with elevated privileges. After obtaining the necessary privileges, it determines
+    whether to launch the GUI or operate as a command-line utility.
+
+    Raises:
+        OSError: If there's an error during administrative privilege check or execution.
+    """
+
+    try:
+        if not is_admin():
+            exit_code = run_as_admin(sys.executable, sys.argv)
+            sys.exit(exit_code)
+    except Exception as ex:
+        raise OSError(f"An exception of type {type(ex).__name__} occurred: {ex}")
 
 
-# root = tk.Tk()
-# frm = ttk.Frame(root, padding=10)
-# frm.grid()
-# ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-# ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-# root.mainloop()
+if __name__ == "__main__":
+    main()
+    # input()
